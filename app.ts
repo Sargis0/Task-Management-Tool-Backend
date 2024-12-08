e339b592cdb1d2745f13993ebb7611c26969f08f
Sargis0 <sargiskirakosyan1994s@gmail.com>
2024-12-08 13:28:59 +0400

    Add database connection class with Mongoose integration

--- app.ts
+++ app.ts
@@ -2,10 +2,21 @@ import 'dotenv/config';
 
 import express, {Express} from 'express';
 
-const PORT = process.env.PORT || 3002;
+import DataBAseConnection from './config/db';
 
-const app:Express = express();
+const PORT: number = process.env.PORT || 3002;
+const app: Express = express();
 
-app.listen(PORT, () => {
-    console.log(`Server listen on port ${PORT}`)
-});
+const start = async (): Promise<void> => {
+    try {
+        await DataBAseConnection.connection(process.env.DB_HOST as string);
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        })
+    } catch (error: object) {
+        console.error('Error occurred while starting the server:', error);
+        process.exit(1);
+    }
+}
+
+start();
---
+++ config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+class DataBAseConnection {
+    async connection(dbURI: string): Promise<void> {
+        try {
+            await mongoose.connect(dbURI);
+            console.log('Connection to the database successfully')
+        } catch (error: object) {
+            console.log('Connection to the database failed: ', error);
+            process.exit(1);
+        }
+    }
+}
+
+export default new DataBAseConnection();
